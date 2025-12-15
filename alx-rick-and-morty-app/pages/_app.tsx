328c9bf22214b9bd7e45f84a59551cd7888dd84a
Davcid1572 <ifejiofordavid1572@gmail.com>
2025-12-15 14:29:22 +0100

    Creating a Component to Test ErrorBoundary

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -5,18 +5,30 @@
 //   return <Component {...pageProps} />;
 // }
 
+// import "@/styles/globals.css";
+// import type { AppProps } from "next/app";
+// import { ApolloProvider } from "@apollo/client";
+// import client from "@/graphql/apolloClient";
+// import ErrorBoundary from "@/components/ErrorBoundary";
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return (
+//     <ErrorBoundary>
+//       <ApolloProvider client={client}>
+//         <Component {...pageProps} />
+//       </ApolloProvider>
+//     </ErrorBoundary>
+//   );
+// }
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ApolloProvider } from "@apollo/client";
-import client from "@/graphql/apolloClient";
 import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary>
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
+      <Component {...pageProps} />
     </ErrorBoundary>
   );
 }
